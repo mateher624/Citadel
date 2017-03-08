94ea38bc0df550cfcd6316ee23a192ec1bfba6ff
mateher624 <mateusz624@student.polsl.pl>
2017-03-09 00:57:30 +0100

    card sprite prototype added

--- Citadel Game/citadelGame/_test_Card.cs
+++ Citadel Game/citadelGame/_test_Card.cs
@@ -9,24 +9,83 @@ using SFML.System;
 
 namespace citadelGame
 {
-    class _test_Card
+    class _test_Card : Drawable
     {
-        public int posx;
-        public int posy;
+        public int start_x;
+        public int start_y;
+        public int width;
+        public int height;
+        public int texture_x;
+        public int texture_y;
+        private int dock_pos_x;
+        private int dock_pos_y;
 
-        private int width;
-        private int height;
+        Texture face;
+        Sprite body;
 
-        private int texturex;
-        private int texturey;
+        public int state;
 
         public Color color;
 
-        public _test_Card(int x, int y, int width, int height, Color color)
+        public bool dock;
+
+        public _test_Card(int start_x, int start_y, int width, int height, Texture face, int texture_x, int texture_y)
         {
-            this.posx = x;
-            this.posy = y;
-            this.color = color;
+            state = 1;
+            this.start_x = start_x;
+            this.start_y = start_y;
+            this.width = width;
+            this.height = height;
+            this.texture_x = texture_x;
+            this.texture_y = texture_y;
+            this.face = face;
+
+
+            this.body = new Sprite();
+            this.body.Texture = this.face;
+            this.body.TextureRect = new IntRect(0, 0, this.width, this.height);
+            this.body.Position = new Vector2f(this.start_x, this.start_y);
+        }
+
+        protected void Update()
+        {
+            if (state == 0) this.body.TextureRect = new IntRect(4 * this.width, 4 * this.height, this.width, this.height);
+            else if (state == 1) this.body.TextureRect = new IntRect(texture_x * this.width, texture_y * this.height, this.width, this.height);
+        }
+
+        public void Collide(int x, int y)
+        {
+            if (dock == true)
+            {
+                this.start_x = x - dock_pos_x;
+                this.start_y = y - dock_pos_y;
+                this.body.Position = new Vector2f(this.start_x, this.start_y);
+            }
+        }
+
+        public void Clicked(int x, int y, Mouse.Button button)
+        {
+            if (x >= this.start_x && x <= (this.start_x + width) && y >= this.start_y && y <= (this.start_y + height) && button.ToString() == "Left")
+            {
+                dock = true;
+                dock_pos_x = x - this.start_x;
+                dock_pos_y = y - this.start_y;
+
+                //if (state == 0) state = 1;
+                //else state = 0;
+            }
+            
+        }
+
+        public void UnClicked(int x, int y, Mouse.Button button)
+        {
+            dock = false;
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            Update();
+            target.Draw(body, states);
         }
     }
 }
--- Citadel Game/citadelGame/_test_RPG.cs
+++ Citadel Game/citadelGame/_test_RPG.cs
@@ -15,32 +15,45 @@ namespace citadelGame
         _test_Tilemap map;
         Texture tileset;
         Texture btn_face;
+        Texture deck;
         List<UIButton> button_list;
+        List<_test_Card> card_sprite_list;
 
         public _test_RPG() : base(800, 600, "Game Name", Color.Cyan)
         {
             button_list = new List<UIButton>();
+            card_sprite_list = new List<_test_Card>();
         }
 
         protected override void CheckCollide(MouseMoveEventArgs e)
         {
-            foreach (UIButton button in button_list) button.Collide(e.X, e.Y);
+            Vector2i mouse_coords = new Vector2i(e.X, e.Y);
+            Vector2f world_coords = window.MapPixelToCoords(mouse_coords);
+            foreach (UIButton button in button_list) button.Collide((int)world_coords.X, (int)world_coords.Y);
+            foreach (_test_Card card in card_sprite_list) card.Collide((int)world_coords.X, (int)world_coords.Y);
         }
 
         protected override void CheckClick(MouseButtonEventArgs e)
         {
-            foreach (UIButton button in button_list) button.Clicked(e.X, e.Y, e.Button);
+            Vector2i mouse_coords = new Vector2i(e.X, e.Y);
+            Vector2f world_coords = window.MapPixelToCoords(mouse_coords);
+            foreach (UIButton button in button_list) button.Clicked((int)world_coords.X, (int)world_coords.Y, e.Button);
+            foreach (_test_Card card in card_sprite_list) card.Clicked((int)world_coords.X, (int)world_coords.Y, e.Button);
         }
 
         protected override void CheckUnClick(MouseButtonEventArgs e)
         {
-            foreach (UIButton button in button_list) button.UnClicked(e.X, e.Y, e.Button);
+            Vector2i mouse_coords = new Vector2i(e.X, e.Y);
+            Vector2f world_coords = window.MapPixelToCoords(mouse_coords);
+            foreach (UIButton button in button_list) button.UnClicked((int)world_coords.X, (int)world_coords.Y, e.Button);
+            foreach (_test_Card card in card_sprite_list) card.UnClicked((int)world_coords.X, (int)world_coords.Y, e.Button);
         }
 
         protected override void LoadContent()
         {
             tileset = new Texture("Resources/DungeonTileset.png");
             btn_face = new Texture("Resources/btn_play.bmp");
+            deck = new Texture("Resources/deck.gif");
         }
 
         protected override void Initialize()
@@ -54,6 +67,10 @@ namespace citadelGame
             UIGlyphButton button_off = new UIGlyphButton(320, 240, 95, 53, btn_face);
             button_off.state = -1;
             button_list.Add(button_off);
+
+            card_sprite_list.Add(new _test_Card(1, 1, 72, 100, deck, 0, 2));
+            card_sprite_list.Add(new _test_Card(37, 51, 72, 100, deck, 1, 2));
+            card_sprite_list.Add(new _test_Card(73, 101, 72, 100, deck, 2, 2));
         }
 
         protected override void Tick()
@@ -67,6 +84,10 @@ namespace citadelGame
             foreach (UIButton button in button_list)
             {
                 window.Draw(button);
+            }
+            foreach (_test_Card card in card_sprite_list)
+            {
+                window.Draw(card);
             }
         }
     }
