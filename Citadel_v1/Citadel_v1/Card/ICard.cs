3837e8c93aace996db8b434d8e8239a5dd0ee8a2
filmus <filip.musiol@op.pl>
2017-05-29 12:36:56 +0200

    update_5

--- Citadel_v1/Citadel_v1/Card/ICard.cs
+++ Citadel_v1/Citadel_v1/Card/ICard.cs
@@ -8,6 +8,9 @@ namespace Citadel_v1
 {
     interface ICard
     {
+        string TextureSourceFileName { get; }
+        int CoordinateY { get; }
+        int CoordinateX { get; }
         string Name { get; }
         int Id { get; }
     }
